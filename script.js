6ed240883265ec7fbc6cbac16a86def17a900cea
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-06-18 20:51:03 +0530

    Progress is working now

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let songIndex = 0;
 let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+
 
 
 
@@ -30,18 +32,28 @@ masterPlay.addEventListener('click', ()=>
         audioElement.play()
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
     }
 
     else{
         audioElement.pause()
         masterPlay.classList.remove('fa-pause-circle');
         masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity =0;
     }
 
 })
 
 // Listen to Events
-myProgressBar.addEventListener('timeupdate', ()=> {
-console.log('timeupdate');
-// update seekbar.
+audioElement.addEventListener('timeupdate', ()=> {
+    // console.log('timeupdate');
+
+    // update seekbar.
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value = progress;
+
+    //change song duration by seekbar.
+    myProgressBar.addEventListener('change', ()=> {
+        audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+    })
 })
